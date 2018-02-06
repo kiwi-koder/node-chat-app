7141e603c7ed2c8144ffc18b97c6cea54a74e413
Joshua Rippon <joshua@pomerolpartners.com>
2018-02-06 09:44:42 +0000

    Greet new user, alert others

--- public/js/index.js
+++ public/js/index.js
@@ -12,3 +12,11 @@ socket.on('disconnect', function() {
 socket.on('newMessage', function(newMessage) {
     console.log('New Message', newMessage);
 });
+
+socket.on('welcome', function(welcome) {
+    console.log('Welcome', welcome);
+});
+
+socket.on('newJoin', function(newJoin) {
+    console.log('New Join', newJoin);
+})
