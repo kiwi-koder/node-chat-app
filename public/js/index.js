fd80105e462bed94332694553068e436106e93ab
Joshua Rippon <joshua@pomerolpartners.com>
2018-02-06 13:56:24 +0000

    Add styling and geo functionality

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-    
 });
 
 socket.on('disconnect', function() {
@@ -26,6 +25,17 @@ socket.on('newJoin', function(newJoin) {
     console.log('New Join', newJoin);
 })
 
+socket.on('newLocationMessage', function(message) {
+    var li = $('<li></li>');
+    var a = $('<a target="_blank">My current location</a>');
+
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+
+    $('#messages').append(li);
+});
+
 socket.emit('createMessage', {
     from: 'Frank',
     text: 'Hi!'
@@ -35,11 +45,30 @@ socket.emit('createMessage', {
 
 $('#message-form').on('submit', function(e) {
     e.preventDefault();
-
+    var messageTextbox = $('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: messageTextbox.val()
     }, function() {
+        messageTextbox.val('');
+    });
+});
 
+var locationButton = $('#send-location');
+
+locationButton.on('click', function() {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser.');
+    }
+    locationButton.attr('disabled', 'disabled');
+    navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled');
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function() {
+        locationButton.removeAttr('disabled');
+        alert('Unable to fetch location.');
     });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -9,5 +9,17 @@ describe('generateMessage', () => {
         var res = generateMessage(from, text);
         expect(res).toInclude({from, text});
         expect(typeof res.createdAt).toBe('number');
-    })
-})
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = 'Admin';
+        var longitude = 2145152;
+        var latitude = 323523623;
+        var message = generateLocationMessage(from, latitude, longitude);
+        expect(typeof message.createdAt).toBe('number');
+        expect(message.from).toBe(from);
+        expect(message.url).toBe(`https://www.google.com/maps?q=${latitude},${longitude}`);
+    });
+});
